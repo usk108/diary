4543d85f4692e2f0994d665c9995d028d0dbe6b5
Yusuke Toba <toba@ws.cs.kobe-u.ac.jp>
2015-08-16 16:30:33 +0900

    add paginate

--- leaf/app/controllers/records_controller.rb
+++ leaf/app/controllers/records_controller.rb
@@ -1,23 +1,24 @@
 class RecordsController < ApplicationController
   def index
-  	@records = Record.all
+    @records = Record.order("id DESC").limit(7).page(params[:page])
   end
 
   def new
-  	@record = Record.new
+  	# @record = Record.new
+    #  @memo = @record.build_memo
+    # redirect_to action: 'create'
+    @record = Record.create(date: Time.now)
+    redirect_to "/records/#{@record.id}/memos/new"
   end
 
- #  def create
- #  	@record = Record.new(record_params)
-	# @record.save
-	# @memo = Memo.new(record_params[:memos_attributes])
-	# @memo.save
-	# redirect_to records_path
- #  end
   def create
-  	@record = Record.new(record_params)
-	  @record.save
-  	redirect_to records_path
+    # @record = Record.new(record_params)
+    # @record.save
+    @record = Record.create(date: Time.now)
+  	# redirect_to records_path
+    # redirect_to controller: 'memos', :action => "new", :id => @record.id
+    binding.pry
+    redirect_to "/records/#{@record.id}/memos/new"
   end
 
   def edit
--- leaf/app/models/record.rb
+++ leaf/app/models/record.rb
@@ -2,7 +2,7 @@ class Record < ActiveRecord::Base
 	has_one :memo, dependent: :destroy 
     accepts_nested_attributes_for :memo
 
-    # def date_string
-    # 	"#{m.rerord.date.year}年#{m.rerord.date.month}月#{m.rerord.date.day}日"
-    # end
+    def show_date
+    	"#{self.date.year}年#{self.date.month}月#{self.date.day}日"
+    end
 end
