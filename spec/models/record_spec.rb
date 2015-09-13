18ea0f906edbff93331c993d78c50b402f9fc7d5
Yusuke Toba <toba@ws.cs.kobe-u.ac.jp>
2015-09-13 16:15:03 +0900

    set wrong test for checking C CI

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -4,6 +4,6 @@ describe Record do
   it "returns date as a string" do 
     # record = Record.new(date: "2015-09-08 21:10:40 +0900")
     record = build(:record, date: "2015-09-08 21:10:40 +0900")
-    expect(record.show_date).to eq '2015年9月8日'
+    expect(record.show_date).to eq '2015年9月8'
   end
 end
